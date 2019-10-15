ba07411c4cc2e48e37e58cd1280ba359644c00e9
dariusrichardson55 <darius.richardson@me.com>
2019-10-15 14:18:00 +0100

    Create ATM Java.java
    Needs completing (Darius Richardson)

---
+++ Workshop Tasks/Week 4/ATM Java.java
@@ -0,0 +1,28 @@
+class ATM{
+	
+	Account acc=new Account();
+	
+	private float Balance;
+	private float Withdraw;
+	
+	public String GetPin(){
+		return pin;
+	}
+	public void setPin() {
+		this.pin = pin;
+	}
+	
+	public float getBalance(){		
+	    return balance;
+	}	  	
+	public void setWithdraw(float Withdraw) {
+	    this.Withdraw = Withdraw;
+	}
+	
+	public void GetMoney() {
+	    return GetMoney
+    }
+   public void changedeposit() {
+	    this.changedeposit = changedeposit;
+    }
+}
